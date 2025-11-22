ddfee49d5fc61230cd4c32792a6672c5b2572e13
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2025-11-22 08:23:50 +0200

    uncommenting motion routes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routes.vision_routes import router as vision_router
+from routes.motion_routes import router as motion_router
 
-# from routes.motion_routes import router as motion_router
 # from routes.speech_routes import router as speech_router
 # from routes.survey_routes import router as survey_router
 
@@ -9,7 +9,7 @@ app = FastAPI()
 
 # Register APIs
 app.include_router(vision_router, prefix="/vision", tags=["Vision Test"])
-# app.include_router(motion_router, prefix="/motion", tags=["Motion Test"])
+app.include_router(motion_router, prefix="/motion", tags=["Motion Test"])
 # app.include_router(speech_router, prefix="/speech", tags=["Speech Test"])
 # app.include_router(survey_router, prefix="/survey", tags=["Survey Test"])
 
